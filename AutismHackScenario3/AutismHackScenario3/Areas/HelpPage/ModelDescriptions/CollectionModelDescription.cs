05f86b34acb180f3370f0303f27ff5bc7283ab03
Saurav Banerjee <saurav.aec@gmail.com>
2019-04-29 18:31:07 -0400

    Initial Commit

---
+++ AutismHackScenario3/AutismHackScenario3/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AutismHackScenario3.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AutismHackScenario3/AutismHackScenario3/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AutismHackScenario3.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
